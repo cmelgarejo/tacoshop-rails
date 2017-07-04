69b4e2dbd34a28698e02bcb85aa166ec7a2f97a0
Christian Melgarejo Bresanovich <cmelgarejo.dev@gmail.com>
2017-07-04 13:09:43 -0400

    Changed type for sides as booleans

--- db/migrate/20170703184813_create_tacos.rb
+++ db/migrate/20170703184813_create_tacos.rb
@@ -2,9 +2,9 @@ class CreateTacos < ActiveRecord::Migration[5.1]
   def change
     create_table :tacos do |t|
       t.string :meat
-      t.string :rice
-      t.string :salsa
-      t.string :fries
+      t.boolean :rice
+      t.boolean :salsa
+      t.boolean :fries
       t.string :notes
 
       t.timestamps
